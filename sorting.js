9fafd312c2343063dd6d35a3631ef15995d91080
justinwasserstein <justin.wasserstein@gmail.com>
2020-12-09 16:35:53 -0500

    Add quickSort2

--- sorting.js
+++ sorting.js
@@ -1,7 +1,6 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(nums){
-    const arr = nums.slice();
+function bubbleSort(arr){
     let lengthToSort = arr.length;
     let firstIdx = 0;
     let secondIdx = 1;
@@ -20,8 +19,7 @@ function bubbleSort(nums){
     return arr;
 }
 
-function selectionSort(nums){
-    const arr = nums.slice();
+function selectionSort(arr){
     let smallestIdx = 0;
     let startingIdx = 0;
     while(startingIdx < arr.length - 1){
@@ -37,8 +35,7 @@ function selectionSort(nums){
     return arr;
 }
 
-function insertionSort(nums){
-    const arr = nums.slice();
+function insertionSort(arr){
     for(let currentIdx = 1; currentIdx < arr.length; currentIdx++){
         if(arr[currentIdx] < arr[currentIdx - 1]){
             let sortIdx = currentIdx - 1;
@@ -52,8 +49,7 @@ function insertionSort(nums){
     return arr;
 }
 
-function insertionSortSwap(nums){ // faster to just swap than insert/delete
-    const arr = nums.slice();
+function insertionSortSwap(arr){ // faster to just swap than insert/delete
     for(let currentIdx = 1; currentIdx < arr.length; currentIdx++){
         if(arr[currentIdx] < arr[currentIdx - 1]){
             let b = currentIdx;
@@ -68,11 +64,11 @@ function insertionSortSwap(nums){ // faster to just swap than insert/delete
     return arr;
 }
 
-function mergeSort(nums){
-    if(nums.length < 2) return nums;
+function mergeSort(arr){
+    if(arr.length < 2) return arr;
 
-    const firstHalf = nums.slice(0, Math.floor(nums.length/2));
-    const secondHalf = nums.slice(Math.floor(nums.length/2));
+    const firstHalf = arr.slice(0, Math.floor(arr.length/2));
+    const secondHalf = arr.slice(Math.floor(arr.length/2));
     
     return merge(mergeSort(firstHalf), mergeSort(secondHalf));
 }
@@ -99,12 +95,11 @@ function merge(arr1, arr2){
     return mergedArr;
 }
 
-function quickSort(nums){
-    if(nums.length < 2){
-        return nums;
+function quickSort(arr){  // update to sort in place instead of creating new subarrays.  Use two pointers moving inward swapping when left > right
+    if(arr.length < 2){
+        return arr;
     }
 
-    const arr = nums.slice();
     let pivot = arr.length - 1;
     for(let i = pivot - 1; i >= 0; i--){
         if(arr[i] > arr[pivot]){
@@ -118,14 +113,55 @@ function quickSort(nums){
     return quickSort(first).concat([arr[pivot]], quickSort(second));
 }
 
-console.time('generation');
-const bigArr = Array(2000000).fill(0).map(() => Math.floor(Math.random()*1000));
-console.timeEnd('generation');
+function quickSort2(arr, left, right){ // left and right bounds are inclusive
+    left = left !== undefined ? left : 0;
+    right = right !== undefined ? right : (arr.length - 1);
 
-console.time('merge');
-mergeSort(bigArr);
-console.timeEnd('merge');
+    if(right - left < 1){
+        return;
+    }
+
+    let pivot = right;
+    let lp = left;
+    let rp = right - 1;
+    while(lp <= rp){
+        if(arr[lp] <= arr[pivot] && arr[rp] >= arr[pivot]){
+            lp++;
+            rp--;
+        } else if(arr[lp] >= arr[pivot] && arr[rp] <= arr[pivot]){
+            [arr[lp], arr[rp]] = [arr[rp], arr[lp]];
+        } else if(arr[lp] <= arr[pivot] && arr[rp] <= arr[pivot]){
+            lp++;
+        } else {
+            rp--;
+        }
+    }
+    [arr[pivot], arr[lp]] = [arr[lp], arr[pivot]];
+    pivot = lp;
+
+    quickSort2(arr, left, pivot - 1);
+    quickSort2(arr, pivot + 1, right);
+}
+
+console.time('generation');
+const bigArr = Array(1000000).fill(0).map(() => Math.floor(Math.random()*1000));
+const bigArr2 = bigArr.slice();
+const bigArr3 = bigArr.slice();
+const bigArr4 = bigArr.slice();
+console.timeEnd('generation');
 
 console.time('quick');
 quickSort(bigArr);
 console.timeEnd('quick');
+
+console.time('quick2');
+quickSort2(bigArr2);
+console.timeEnd('quick2');
+
+console.time('merge');
+mergeSort(bigArr3);
+console.timeEnd('merge');
+
+console.time('builtin');
+bigArr4.sort((a, b) => a - b);
+console.timeEnd('builtin');
